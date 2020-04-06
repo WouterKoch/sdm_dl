8f187837eaf9f0217adfca4d3e13d80cb03ee5c1
mrademaker <48674159+mrademaker@users.noreply.github.com>
2020-04-06 17:07:01 +0200

    Update usage.py
    general path structure (only username needs to be specified)

--- usage.py
+++ usage.py
@@ -2,20 +2,23 @@ import os
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+user= 'wouter' #'markrademaker'
+
 # Remember to set the environment variables
 os.environ["RASTER_MAX_LAT"] = "54"
 os.environ["RASTER_MIN_LAT"] = "50"
 os.environ["RASTER_MIN_LON"] = "3"
 os.environ["RASTER_MAX_LON"] = "8"
 os.environ["RASTER_CELL_SIZE_DEG"] = str(1 / 120)
-os.environ["PROJECT_ROOT"] = "/home/wouter/Projects/Naturalis/sdm_dl"
+os.environ["PROJECT_ROOT"] = "/home/{}/Projects/Naturalis/sdm_dl".format(user)
 
-os.environ["GLOBE_FILE_PATH"] = "/home/wouter/Projects/Naturalis/environment/GLOBE/c10g"
-os.environ["ESACCI_FOLDER_PATH"] = "/home/wouter/Projects/Naturalis/environment/ESACCI"
-os.environ["WORLDCLIM_FOLDER_PATH"] = "/home/wouter/Projects/Naturalis/environment/WorldClim"
-os.environ["BIOCLIM_FOLDER_PATH"] = "/home/wouter/Projects/Naturalis/environment/BioClim"
+os.environ["BALTIC_FILE_PATH"] = "/home/{}/Projects/Naturalis/environment/BALTIC".format(user)
+os.environ["GLOBE_FILE_PATH"] = "/home/{}/Projects/Naturalis/environment/GLOBE/c10g".format(user)
+os.environ["ESACCI_FOLDER_PATH"] = "/home/{}/Projects/Naturalis/environment/ESACCI".format(user)
+os.environ["WORLDCLIM_FOLDER_PATH"] = "/home/{}/Projects/Naturalis/environment/WorldClim".format(user)
+os.environ["BIOCLIM_FOLDER_PATH"] = "/home/{}/Projects/Naturalis/environment/BioClim".format(user)
 
-os.environ["RASTER_CACHE_FOLDER_PATH"] = "/home/wouter/Projects/Naturalis/environment/sdm_dl_cache"
+os.environ["RASTER_CACHE_FOLDER_PATH"] = "/home/{}/Projects/Naturalis/environment/sdm_dl_cache".format(user)
 
 import get_environmental_layer as get_env
 
