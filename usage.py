ed0547a2cf14420c4ae708f72f27b4ac3b5a1bed
Wouter <wouter.koch@artsdatabanken.no>
2020-04-09 20:30:20 +0200

    Fix project root path

--- usage.py
+++ usage.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-user= 'Users/markrademaker'#'home/wouter'
+user = 'Users/markrademaker'  # 'home/wouter'
 
 # Remember to set the environment variables
 os.environ["RASTER_MAX_LAT"] = "60"
@@ -10,7 +10,7 @@ os.environ["RASTER_MIN_LAT"] = "52"
 os.environ["RASTER_MIN_LON"] = "9"
 os.environ["RASTER_MAX_LON"] = "24"
 os.environ["RASTER_CELL_SIZE_DEG"] = str(1 / 120)
-os.environ["PROJECT_ROOT"] = "{}/Projects/Naturalis/sdm_dl".format(user)
+os.environ["PROJECT_ROOT"] = "/{}/Projects/Naturalis/sdm_dl".format(user)
 
 os.environ["BALTIC_FILE_PATH"] = "/{}/Projects/Naturalis/environment/BALTIC".format(user)
 os.environ["GLOBE_FILE_PATH"] = "/{}/Projects/Naturalis/environment/GLOBE/c10g".format(user)
@@ -23,14 +23,15 @@ os.environ["RASTER_CACHE_FOLDER_PATH"] = "/{}/Projects/Naturalis/environment/sdm
 import get_environmental_layer as get_env
 
 from layer_readers import baltic_sea as layer_reader
+
 maps = get_env.get_blocks([(56.1, 12, datetime(1991, 1, 6))], 25, layer_reader)
 
 for position in maps:
     for name, map in position.items():
-        fig,ax = plt.subplots()
-        im = ax.imshow(map,cmap=plt.get_cmap('hot'))
+        fig, ax = plt.subplots()
+        im = ax.imshow(map, cmap=plt.get_cmap('hot'))
         print(map)
         fig.colorbar(im)
-        #plt.imshow(map)
+        # plt.imshow(map)
         plt.title(name)
         plt.show()
