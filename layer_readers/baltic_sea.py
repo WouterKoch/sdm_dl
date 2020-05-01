aacfe28a89860f2ac42b3340181c2d7b9bec5106
mrademaker <48674159+mrademaker@users.noreply.github.com>
2020-05-01 10:18:38 +0200

    baltic_layer_reader fixed indexes
    Fixed the issue with fill blocks not finding correct locations due to index issues resulting in np.nan filled blocks;
    to do this , I adjusted the get_array_from_tiff and get_value_from_array_functions. Now it rebuilds an index for the baltic sea maps and is able to extract the value of the array.

--- layer_readers/baltic_sea.py
+++ layer_readers/baltic_sea.py
@@ -18,11 +18,13 @@ baltic_vars = ['Clupea_harengus_JUV', 'Clupea_harengus_SA', 'Clupea_harengus_LA'
 
 baltic_depth = ['(0, 20)', '(20, 70)', '(70, 460)']
 
-
+###Traditional
 def get_array_from_tif(_, baltic_variable):
     path = os.environ["BALTIC_FILE_PATH"]
     ds = gdal.Open(os.path.join(path, baltic_variable + '.tif'))
     band = ds.GetRasterBand(1)
+    #print(np.max(band.ReadAsArray()))
+    #try and extract value in one go
     return band.ReadAsArray()
 
 
@@ -30,15 +32,48 @@ def get_value_from_array(lat, lon, cci_array, array_height, array_width):
     lat_pos = int((array_height / 2) - (lat * (array_height / 180)))
     lon_pos = int((array_width / 2) + (lon * (array_width / 360)))
     value = cci_array[lat_pos][lon_pos]
-    if value < -1000000:
-        return np.nan
+    #if value < -500:
+    #    return np.nan
     return value
+#############################
+# Try this approach instead #
+#############################
+def get_band_from_tif(_,baltic_variable):
+    path = os.environ["BALTIC_FILE_PATH"]
+    ds = gdal.Open(os.path.join(path, baltic_variable + '.tif'))
+    band = ds.GetRasterBand(1)
 
+    cols = ds.RasterXSize
+    rows = ds.RasterYSize
+
+    transform = ds.GetGeoTransform()
+
+    xOrigin = transform[0]
+    yOrigin = transform[3]
+    pixelWidth = transform[1]
+    pixelHeight = transform[5]
+
+    array = band.ReadAsArray(0,0,cols,rows)
+    return array,xOrigin,yOrigin,pixelWidth,pixelHeight
+
+def get_value_from_band(lat,lon,inp_array,xOrigin,yOrigin,pixelWidth,pixelHeight):
+    col = int((lon - xOrigin) / pixelWidth)
+    row = int((lat - yOrigin) / pixelHeight)
+   # print(inp_array.shape,'\n',
+    #      lat,'\n',
+    #      lon,'\n',
+    #      xOrigin,'\n',
+    #      yOrigin,'\n',
+    #      col,'\n',
+    #      row,'\n')
+    return inp_array[row,col]
+####################
 
 def get_layer_names(time):
     filenames = os.listdir(os.environ["BALTIC_FILE_PATH"])
     suffix = '.tif'
     year = str(time.year)
+    #print(year)
     layer_names = []
 
     for fn in filenames:
@@ -49,7 +84,7 @@ def get_layer_names(time):
 
 def get_layer_from_file(layer_name):
     filename = os.path.join(os.getenv("RASTER_CACHE_FOLDER_PATH"), 'baltic', layer_name + '.npz')
-
+    #print(filename)
     raster_max_lat = int(os.getenv("RASTER_MAX_LAT"))
     raster_min_lat = int(os.getenv("RASTER_MIN_LAT"))
     raster_max_lon = int(os.getenv("RASTER_MAX_LON"))
@@ -60,6 +95,8 @@ def get_layer_from_file(layer_name):
 
     if os.path.exists(filename):
         layer = dict(np.load(filename, allow_pickle=True))
+        #print(layer)
+        #print('exists')
         layer['metadata'] = layer['metadata'].item()
     else:
         raster_width = int((raster_max_lon - raster_min_lon) / raster_cell_size_deg) + 1
@@ -80,11 +117,16 @@ def fill_blocks(layer_name, to_fetch, cell_size_degrees):
         return [None] * len(to_fetch)
 
     path = os.getenv("BALTIC_FOLDER_PATH")
-    array = get_array_from_tif(path, layer_name)  # [-2:])
-    array_height, array_width = array.shape
+    #array = get_array_from_tif(path, layer_name)  # [-2:])
+    inp_array, xOrigin, yOrigin, pixelWidth, pixelHeight = get_band_from_tif(path,layer_name)
+    #array_height, array_width = array.shape
+    #print(array.shape)
+    #print(np.min(array),np.max(array))
     result = []
 
+    #print(len(to_fetch))
     for block_index in range(len(to_fetch)):
+        #print(block_index)
         if to_fetch[block_index] == None:
             result += [None]
             continue
@@ -95,9 +137,22 @@ def fill_blocks(layer_name, to_fetch, cell_size_degrees):
         for row in range(block_height):
             for col in range(block_width):
                 if np.isnan(block[row, col]):
-                    block[row, col] = get_value_from_array(lat_start - (row * cell_size_degrees),
-                                                           lon_start + (col * cell_size_degrees), array,
-                                                           array_height,
-                                                           array_width)
+                    #block[row, col] = get_value_from_array(lat_start - (row * cell_size_degrees),
+                    #                                       lon_start + (col * cell_size_degrees), array,
+                    #                                       array_height,
+                    #                                       array_width)
+                    block[row,col] = get_value_from_band(lat=lat_start - (row * cell_size_degrees),
+                                                         lon= lon_start + (col * cell_size_degrees),
+                                                         inp_array=inp_array,
+                                                         xOrigin=xOrigin,
+                                                         yOrigin=yOrigin,
+                                                         pixelWidth=pixelWidth,
+                                                         pixelHeight=pixelHeight)
+                    #print(lat_start - (row * cell_size_degrees),lon_start + (col * cell_size_degrees), np.quantile(array,0.8),array_height,array_width)
+                    #print(get_value_from_array(lat_start - (row * cell_size_degrees),
+                    #                                       lon_start + (col * cell_size_degrees), array,
+                    #                                       array_height,
+                    #                                       array_width))
         result += [block]
+    #print(result)
     return result
